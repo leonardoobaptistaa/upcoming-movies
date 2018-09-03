c0e754d266b7660b08865f99cf15926eb2273790
Leonardo Baptista <leonardoobaptistaa@gmail.com>
2018-09-03 11:58:55 -0300

    Initial commit

---
+++ Upcoming Movies/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Upcoming Movies
+//
+//  Created by Leonardo Baptista on 9/3/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
